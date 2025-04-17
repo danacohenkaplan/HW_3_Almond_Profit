dc9cc5bf6b5271ecf40af5f76d34405f256fbd43
danacohenkaplan <dcohen-kaplan@bren.ucsb.edu>
2025-04-17 13:08:29 -0700

    First crack at the inital function (Almond_Yield.R)

---
+++ Scripts/Almond_Profit.R
@@ -0,0 +1,16 @@
+#' computes profit from almond yield
+#' @param baseline_yield average yield (tons/acre) 
+#' @param price ($/ton)
+#' @param yield (tons/acre) - corresponds to a year
+#' @param profit_margin (percent of sales as profit)
+#' @return baseline_profit ($/acre) data frame with estimate of profit
+
+profit_from_yield <- function(baseline_yield = 1, price = 1000, profit_margin = .5, yield_anomaly) {
+  #calculate power
+  baseline_profit <-  (baseline_yield + yield) * price * profit_margin 
+  
+  
+  return(baseline_profit)
+  
+}
+  
