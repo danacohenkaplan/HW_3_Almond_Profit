27f88b20af94716e7adb90394cbea225300c1c7a
hstrecker <henrystrecker@ucsb.edu>
2025-04-21 12:19:46 -0700

    Sensitivity Analysis Results

--- Scripts/Almond_Profit.R
+++ Scripts/Almond_Profit.R
@@ -5,9 +5,9 @@
 #' @param profit_margin (percent of sales as profit)
 #' @return baseline_profit ($/acre) data frame with estimate of profit
 
-profit_from_yield <- function(baseline_yield = 1, price = 1000, profit_margin = .5, yield_anomaly) {
+profit_from_yield <- function(baseline_yield = 0.9, price = 1000, profit_margin = .5, yield_anomaly) {
   #calculate power
-  baseline_profit <-  (baseline_yield + yield) * price * profit_margin 
+  baseline_profit <-  (baseline_yield + yield_anomaly) * price * profit_margin 
   
   
   return(baseline_profit)
